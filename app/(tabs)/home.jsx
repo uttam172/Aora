770d65b2b7c00ee060d7e16e144e8a2ebd6063d1
uttam172 <uttamdobariya17@gmail.com>
2024-08-13 21:50:27 +0530

    home screen complete

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,26 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useState } from 'react'
+
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+
+    const [refreshing, setRefreshing] = useState(false)
+
+    const onRefresh = async () => {
+        setRefreshing(true)
+        // recall videos -> if any new video appear
+        setRefreshing(false)
+    }
+
     return (
-        <SafeAreaView className="bg-primary">
+        <SafeAreaView className="bg-primary h-full">
             <FlatList
-                data={[{ id: 1 }, { id: 2 }, { id: 3 },]}
+                data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
                 keyExtractor={(item) => item.$id}
                 renderItem={({ item }) => (
                     <Text className="text-3xl text-white">{item.id}</Text>
@@ -17,8 +30,12 @@ const Home = () => {
 
                         <View className="justify-between items-start flex-row mb-6">
                             <View>
-                                <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-                                <Text className="text-2xl font-psemibold text-white">Uttam</Text>
+                                <Text className="font-pmedium text-sm text-gray-100">
+                                    Welcome Back
+                                </Text>
+                                <Text className="text-2xl font-psemibold text-white">
+                                    Uttam
+                                </Text>
                             </View>
                             <View className="mt-1.5">
                                 <Image
@@ -32,11 +49,24 @@ const Home = () => {
                         <SearchInput />
 
                         <View className="w-full flex-1 pt-5 pb-8">
-                            <Text className="text-gray-100 text-lg font-pregular mb-3">Latest Videos</Text>
+                            <Text className="text-gray-100 text-lg font-pregular mb-3">
+                                Latest Videos
+                            </Text>
+
+                            <Trending
+                                posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []}
+                            />
                         </View>
 
                     </View>
                 )}
+                ListEmptyComponent={() => (
+                    <EmptyState
+                        title="No Videos Found"
+                        subtitle="Be the first one to upload a video"
+                    />
+                )}
+                refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
             />
         </SafeAreaView>
     )
---
+++ components/Trending.jsx
@@ -0,0 +1,19 @@
+
+import { View, Text, FlatList } from 'react-native'
+
+const Trending = ({ posts }) => {
+
+    return (
+        <FlatList
+            data={posts}
+            keyExtractor={(item) => item.$id}
+            renderItem={({ item }) => (
+                <Text className="text-3xl text-white">{item.id}</Text>
+            )}
+            horizontal
+        />
+    )
+}
+
+export default Trending
+
