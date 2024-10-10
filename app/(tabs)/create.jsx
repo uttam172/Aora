ea79c38b5a74ace1dc894986b2b8b8c72dce4778
uttam172 <uttamdobariya17@gmail.com>
2024-10-10 19:29:13 +0530

    remain to solve '$id undefine' error

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import { View, Text, ScrollView, TouchableOpacity, Image, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Video, ResizeMode } from 'expo-av'
-import * as DocumentPicker from 'expo-document-picker'
+import * as ImagePicker from 'expo-image-picker'
 
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 
 import { icons } from '../../constants'
+import { router } from 'expo-router'
+import { createVideo } from '../../lib/appwrite'
+import { useGlobalContext } from  '../../context/GlobalProvider'
+
 
 const Create = () => {
+
+    const { user } = useGlobalContext()
 
     const [uploading, setUploading] = useState(false)
 
@@ -21,10 +27,11 @@ const Create = () => {
     })
 
     const openPicker = async (selectType) => {
-        const result = await DocumentPicker.getDocumentAsync({
-            type: selectType === 'image'
-                ? ['image/png', 'image/jpg']
-                : ['video/mp4', 'video/gif']
+        let result = await ImagePicker.
+        launchImageLibraryAsync({
+            mediaTypes: selectType === 'image' ? ImagePicker.MediaTypeOptions.Images : ImagePicker.MediaTypeOptions.Videos,
+            aspect: [4, 3],
+            quality: 1
         })
 
         if (!result.canceled) {
@@ -34,14 +41,36 @@ const Create = () => {
             if (selectType === 'video') {
                 setForm({ ...form, video: result.assets[0] })
             }
-        } else {
-            setTimeout(() => {
-                Alert.alert('Document Picked', JSON.stringify(result, null ,2))
-            }, 100)
-        }
+        } 
     }
 
-    const submit = () => { }
+    const submit = async () => {
+        if (!form.prompt || !form.title || !form.thumbnail || !form.video) {
+            return Alert.alert('Please fill in all the fields')
+        }
+
+        setUploading(true)
+
+        try {
+            await createVideo({
+                ...form, userId: user.$id
+            })
+
+            Alert.alert('Success', 'Post uploaded successfully')
+            router.push('/home')
+        } catch (error) {
+            Alert.alert('Errorid', error.message)
+        } finally {
+            setForm({
+                title: '',
+                video: null,
+                thumbnail: null,
+                prompt: ''
+            })
+
+            setUploading(false)
+        }
+    }
 
     return (
         <SafeAreaView className="bg-primary h-full">
@@ -67,10 +96,8 @@ const Create = () => {
                         {form.video ? (
                             <Video
                                 source={{ uri: form.video.uri }}
-                                className="w-full h-64 rounded-2xl"
-                                useNativeControls
+                                className="w-full h-64 rounded-2xl"                                
                                 resizeMode={ResizeMode.COVER}
-                                isLooping
                             />
                         ) : (
                             <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
